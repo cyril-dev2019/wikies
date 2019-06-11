cd629e9f674bd3babd813cd4ad26564bf6b7202e
cyril-dev2019 <cyril.marienely@gmail.com>
2019-06-11 15:18:20 +0200

    fetch category list from db

--- pdo.php
+++ pdo.php
@@ -1,5 +1,7 @@
 <?php
 
-$pdo = new PDO("mysql:host=localhost;dbname=WIKIES", "root", "")
+$pdo = new PDO("mysql:host=localhost;dbname=WIKIES", "root", "");
+
+$categories = $pdo->query('SELECT * FROM categorie')->fetchAll();
 
 ?>
